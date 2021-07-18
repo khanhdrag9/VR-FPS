0070ccd5f4790dd077653a767b885131088f878d
khanhdrag9 <tkhanh783@gmail.com>
2021-07-18 18:17:53 +0700

    controll override able

--- Assets/Game/Scripts/InputManager.cs
+++ Assets/Game/Scripts/InputManager.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class InputManager : MonoBehaviour
 {
+    public delegate void InputUpdate();
+
     public bool Active {get; set; } = true;
 
     public bool IsFire {get; set;} = false;
@@ -12,7 +14,8 @@ public class InputManager : MonoBehaviour
     public float MouseY {get; set;} = 0f;
     public Vector3 Move {get; set;} = Vector3.zero;
 
-    void Update()
+    public InputUpdate Current = null;
+    public InputUpdate DefaultUpdate => ()=>
     {
         IsFire = GetButtonDown("Fire1");
         IsJump = GetButtonDown("Jump");
@@ -20,6 +23,18 @@ public class InputManager : MonoBehaviour
         MouseY = GetAxis("Mouse Y");
 
         Move = new Vector3(GetAxis("Horizontal"), 0, GetAxis("Vertical"));
+    };
+
+    public InputUpdate Empty => () => {};
+
+    void Start()
+    {
+        if(Current == null) Current = DefaultUpdate;
+    }
+
+    void Update()
+    {
+        Current.Invoke();
     }
 
     bool GetButtonDown(string name) => Active && Input.GetButtonDown(name);
