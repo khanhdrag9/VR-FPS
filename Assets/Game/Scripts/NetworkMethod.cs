ceb8dbb5c59115ed9a066ddf4ea853f3119a9757
khanhdrag9 <tkhanh783@gmail.com>
2021-07-19 01:02:01 +0700

    Rpc for shooting, improved flow

--- Assets/Game/Scripts/NetworkMethod.cs
+++ Assets/Game/Scripts/NetworkMethod.cs
@@ -6,7 +6,11 @@ using Bolt;
 
 public class NetworkMethod : NetworkBehaviour
 {
+    [SerializeField] public Camera playerCamera = null;
+    [SerializeField] public Camera weaponCamera = null;
     [SerializeField] Transform weaponPlace = null;
+    [SerializeField] InputManager input = null;
+
     [SerializeField] [SyncVar] string currentWeapon;
 
     public string CurrentWeapon => currentWeapon;
@@ -23,6 +27,17 @@ public class NetworkMethod : NetworkBehaviour
 
     public void Shoot()
     {
-        
+        CmdShoot(netId);
+    }
+
+    [Command(requiresAuthority = false)] void CmdShoot(uint from)
+    {
+        RpcShoot(from);
+    }
+
+    [ClientRpc] void RpcShoot(uint from)
+    {
+        if(netId != from)
+            CustomEvent.Trigger(gameObject, "RpcShoot");  
     }
 }
